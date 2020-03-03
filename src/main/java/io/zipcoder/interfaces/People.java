ff1f8610d42ec8f137a47725b330b0b091b85637
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-03 14:01:18 -0500

    most of People complete

---
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -0,0 +1,68 @@
+package io.zipcoder.interfaces;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class People<E> implements Iterable<E>{
+    ArrayList<Person> personList = new ArrayList<>();
+
+    public void add(Person person){
+        this.personList.add(person);
+    }
+
+    public Person findById(Long id){
+        for(Person person : personList){
+            if (person.getId() == id){
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public boolean contains(Person person){
+        return personList.contains(person);
+    }
+
+    public void remove(Person person){
+        personList.remove(person);
+    }
+
+    public void remove(long id){
+        personList.remove(findById(id));
+    }
+
+    public void removeAll(){
+        personList.clear();
+    }
+
+    public int count(){
+        return personList.size();
+    }
+
+    public Person[] toArray(){
+        Person[] result = new Person[personList.size()];
+        int i = 0;
+        for(Person person : personList){
+            result[i] = person;
+            i++;
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return null;
+    }
+}
